eaec240e06bc85c92782f3ba3464cf748c6530bc
Pavel Karasev <pavel.karasev@outlook.com>
2020-03-20 22:32:53 +0100

    Add project files.

---
+++ ObservableVsConverter/LineItem.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObservableVsConverter
+{
+    public class LineItem : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private DateTime _dateOne;
+        public DateTime DateOne
+        {
+            get { return _dateOne; }
+            set
+            {
+                if (_dateOne != value)
+                {
+                    _dateOne = value;
+                    NotifyPropertyChanged(nameof(DateOne));
+                } 
+            }
+        }
+
+        private decimal _cost;
+        public decimal Cost
+        {
+            get { return _cost; }
+            set
+            {
+                if (_cost != value)
+                {
+                    _cost = value;
+                    NotifyPropertyChanged(nameof(Cost));
+                }
+            }
+        }
+
+
+    }
+}
